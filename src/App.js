dc53267fe6aa489f686f1ea51fa1a8f5e78e8bb0
Riccardo Nigrelli <20014421@studenti.uniupo.it>
2019-10-03 19:47:46 +0200

    create form for research

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import './App.css';
+// import FontAwesome from 'react-fontawesome'
+import { Container, Form, Button } from 'react-bootstrap';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+export default class App extends React.Component {
+
+  render() {
+    return (
+      <Container className="App Spacing">
+        <h1>iTunes song list of {this.state.inputValue} </h1>
+        <Form className="Spacing">  
+          <Form.Group controlId="authorName">
+            <Form.Control 
+              deftype="text" 
+              placeholder="Enter author name"
+            />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Search
+          </Button>
+        </Form>
+      </Container>
+    );
+  }
 }
-
-export default App;
