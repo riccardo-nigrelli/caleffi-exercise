62baaeedffb7097b97f1d35f6f0ad08e8ab1d565
Riccardo Nigrelli <20014421@studenti.uniupo.it>
2019-10-03 20:03:16 +0200

    title change like input value

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 // import FontAwesome from 'react-fontawesome'
 import { Container, Form, Button } from 'react-bootstrap';
 
-export default class App extends React.Component {
 
-  render() {
-    return (
-      <Container className="App Spacing">
-        <h1>iTunes song list of {this.state.inputValue} </h1>
-        <Form className="Spacing">  
-          <Form.Group controlId="authorName">
-            <Form.Control 
-              deftype="text" 
-              placeholder="Enter author name"
-            />
-          </Form.Group>
-          <Button variant="primary" type="submit">
-            Search
-          </Button>
-        </Form>
-      </Container>
-    );
-  }
+function App() {
+
+  const [input, setInput] = useState('Post Malone');
+
+  useEffect(() => {
+    document.title = input;
+  });
+
+  return (
+    <Container className='App Spacing'>
+      <h1>iTunes song list of {input}</h1>
+      <Form className='Spacing'>  
+        <Form.Group controlId='authorName'>
+          <Form.Control 
+            defaultValue={input}
+            deftype='text'
+            placeholder='Enter author name'
+            onInput={e => setInput(e.target.value)}
+          />
+        </Form.Group>
+        <Button variant='primary' type='submit'>
+          Search
+        </Button>
+      </Form>
+    </Container>
+  );
 }
+
+export default App;
